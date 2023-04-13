f32a2249529a5abe9416fdba987f2e52b510258e
ZiaurRehmanmalhi <ziamalhi1234@gmail.com>
2023-04-13 11:39:25 +0500

    update

--- selenium bs4 p1.py
+++ selenium bs4 p1.py
@@ -3,7 +3,9 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 driver = webdriver.Chrome()
-driver.get("https://www.youtube.com/@zusmani78/videos")
+# url = input("Enter The Youtube videos URL : ")
+url = "https://www.youtube.com/@zusmani78/videos"
+driver.get(url)
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 all_content = soup.find_all('ytd-rich-item-renderer')
@@ -13,14 +15,16 @@ for videos in all_content:
     thumbnail = videos.find('img', {"style": "background-color: transparent;"}).get('src')
     title = videos.find('yt-formatted-string', {"id": "video-title"}).text
     views = videos.find('span', {"class": "inline-metadata-item style-scope ytd-video-meta-block"}).text
-    upload_time = videos.find('span', {"class": "inline-metadata-item style-scope ytd-video-meta-block"}).next.next
+    upload_time = videos.find('div', {"class": "style-scope ytd-video-meta-block"}).text.strip().split('\n')[-1::-3][0]
+    # upload_time = videos.find('ytd-badge-supported-renderer', {"id": "video-title-link"})
     video_duration = videos.find('span', {"class": "style-scope ytd-thumbnail-overlay-time-status-renderer"}).text.strip()
 
-    all_data = [title, views, video_duration, upload_time, thumbnail]
+    all_data = [title, views, video_duration, thumbnail]
     data.append(all_data)
 
-df = pd.DataFrame(data, columns=["Title", "Views", "Duration", "Upload Time", "Thumbnail"])
-df.to_csv("youtube_videos.csv", index=False)
+    print(upload_time)
+# df = pd.DataFrame(data, columns=["Title", "Views", "Duration", "Upload Time", "Thumbnail"])
+# df.to_csv("youtube_videos.csv", index=False)
 driver.quit()
 
 
